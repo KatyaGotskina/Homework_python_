1be422988f69936261c91d01ac081f8ccf28be55
KatyaGotskina <katya.gotskina@gmail.com>
2022-09-20 18:40:29 +0300

    Добавлена домашняя работа за 12 сентября

---
+++ Домашка за 12.09.py
@@ -0,0 +1,13 @@
+# #Посчитать процент строк, где больше заглавных букв
+str_1 = input().split()
+count1 = 0
+
+for i in str_1:
+    hiigh = 0
+    for j in i:
+        if j.isupper():
+            hiigh += 1                                                
+    if hiigh / len(i) > 0.5:
+        count1 += 1
+
+print(count1/ len(str_1)*100, '%')
