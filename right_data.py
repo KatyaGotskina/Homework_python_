dfec394e7f64b1699feccf3400cc05f18f903411
KatyaGotskina <katya.gotskina@gmail.com>
2022-09-20 18:42:13 +0300

    Добавлен файл с функцией проверки даты

---
+++ right_data.py
@@ -0,0 +1,17 @@
+def data(day, month, year):
+
+    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        days_in_month[1] = 29
+
+    if month > 0 and month < 13:
+
+        return day > 0 and day<= days_in_month[month-1]
+     
+    else:
+        return False
+
+
+
+
